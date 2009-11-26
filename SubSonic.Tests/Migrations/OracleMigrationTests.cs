318150f32fadbfbd75caf9b7fd463e04536bb524
Jeff Valore <rally25rs@yahoo.com>
2009-11-25 21:46:58 -0500

    fixed tests

--- SubSonic.Tests/Migrations/OracleMigrationTests.cs
+++ SubSonic.Tests/Migrations/OracleMigrationTests.cs
@@ -21,21 +21,21 @@ namespace SubSonic.Tests.Migrations
         public void CreateTable_Should_CreateValid_SQL_For_SubSonicTest()
         {
             var shouldbe =
-                @"CREATE TABLE ""SubSonicTests"" (
-  ""Key"" VARCHAR2(16) NOT NULL,
-  ""Thinger"" NUMBER(9,0) NOT NULL,
-  ""Name"" VARCHAR2(255) NOT NULL,
-  ""UserName"" VARCHAR2(500) NOT NULL,
-  ""CreatedOn"" TIMESTAMP NOT NULL,
-  ""Price"" NUMBER(10,2) NOT NULL,
-  ""Discount"" NUMBER(10,2) NOT NULL,
-  ""Lat"" NUMBER(10,3),
-  ""Long"" NUMBER(10,3),
-  ""SomeFlag"" VARCHAR2(1) NOT NULL,
-  ""SomeNullableFlag"" VARCHAR2(1),
-  ""LongText"" CLOB NOT NULL,
-  ""MediumText"" VARCHAR2(800) NOT NULL,
-  CONSTRAINT SubSonicTests_PK PRIMARY KEY (""Key"")
+                @"CREATE TABLE SubSonicTests (
+  Key VARCHAR2(16) NOT NULL,
+  Thinger NUMBER(9,0) NOT NULL,
+  Name VARCHAR2(255) NOT NULL,
+  UserName VARCHAR2(500) NOT NULL,
+  CreatedOn TIMESTAMP NOT NULL,
+  Price NUMBER(10,2) NOT NULL,
+  Discount NUMBER(10,2) NOT NULL,
+  Lat NUMBER(10,3),
+  Long NUMBER(10,3),
+  SomeFlag VARCHAR2(1) NOT NULL,
+  SomeNullableFlag VARCHAR2(1),
+  LongText CLOB NOT NULL,
+  MediumText VARCHAR2(800) NOT NULL,
+  CONSTRAINT SubSonicTests_PK PRIMARY KEY (Key)
 )";
             
             var sql = typeof (SubSonicTest).ToSchemaTable(_provider).CreateSql;
@@ -45,7 +45,7 @@ namespace SubSonic.Tests.Migrations
         [Fact]
         public void DropTable_Should_Create_Valid_Sql()
         {
-            var shouldbe = @"DROP TABLE ""SubSonicTests"" PURGE";
+            var shouldbe = @"DROP TABLE SubSonicTests PURGE";
             
             var sql = typeof(SubSonicTest).ToSchemaTable(_provider).DropSql.Trim();
             Assert.Equal(shouldbe, sql);
@@ -53,7 +53,7 @@ namespace SubSonic.Tests.Migrations
 
         [Fact]
         public void DropColumnSql_Should_Create_Valid_Sql() {
-            var shouldbe = @"ALTER TABLE ""SubSonicTests"" DROP COLUMN UserName";
+            var shouldbe = @"ALTER TABLE SubSonicTests DROP COLUMN UserName";
 
             var sql = typeof (SubSonicTest).ToSchemaTable(_provider).DropColumnSql("UserName");
             Assert.Equal(shouldbe, sql);
@@ -61,7 +61,7 @@ namespace SubSonic.Tests.Migrations
 
         [Fact]
         public void CreateColumnSql_Should_Create_Valid_Sql() {
-            var shouldbe = @"ALTER TABLE ""SubSonicTests"" ADD UserName VARCHAR2(500) DEFAULT ' ' NOT NULL";
+            var shouldbe = @"ALTER TABLE SubSonicTests ADD UserName VARCHAR2(500) DEFAULT ' ' NOT NULL";
 
             var sql = typeof (SubSonicTest).ToSchemaTable(_provider).GetColumn("UserName").CreateSql;
             Assert.Equal(shouldbe, sql);
@@ -69,7 +69,7 @@ namespace SubSonic.Tests.Migrations
 
         [Fact]
         public void AlterColumnSql_Should_Create_Valid_Sql() {
-            var shouldbe = @"ALTER TABLE ""SubSonicTests"" MODIFY UserName VARCHAR2(500) NOT NULL";
+            var shouldbe = @"ALTER TABLE SubSonicTests MODIFY UserName VARCHAR2(500) NOT NULL";
 
             var sql = typeof(SubSonicTest).ToSchemaTable(_provider).GetColumn("UserName").AlterSql;
             Assert.Equal(shouldbe, sql);
