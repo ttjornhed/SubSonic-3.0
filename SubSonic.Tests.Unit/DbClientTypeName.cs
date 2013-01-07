f1f085a316352d8ad4a7f914060bcb7259688c43
Anne Epstein <ajepst@gmail.com>
2013-01-07 13:19:37 -0600

    switching Oracle to odp.net

--- SubSonic.Tests.Unit/DbClientTypeName.cs
+++ SubSonic.Tests.Unit/DbClientTypeName.cs
@@ -11,7 +11,8 @@ namespace SubSonic.Tests.Unit
         public const string MsSqlCe = "System.Data.SqlServerCe.3.5";
         public const string MySql = "MySql.Data.MySqlClient";
         //public const string OleDb = "System.Data.OleDb";
-        public const string OracleDataAccess = "System.Data.OracleClient";
+        public const string Oracle = "System.Data.OracleClient";
+        public const string OracleDataAccess = "Oracle.DataAccess.Client";
         public const string SqlLite = "System.Data.SQLite";
         public const string DB2 = "IBM.Data.DB2";
     }
