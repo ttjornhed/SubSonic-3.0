19a0eb32a7270855864133ef33151749928a5720
Alonso Robles <alonso@alonsorobles.com>
2013-01-09 19:33:34 -0600

    Added castle windosor container to help with type converter injection. Added concept of IValueTypeConverters to handle specfic SQL type to .NET convertions

---
+++ SubSonic.Core/Configuration/Configurator.cs
@@ -0,0 +1,47 @@
+﻿using Castle.MicroKernel.Registration;
+using Castle.Windsor;
+using SubSonic.TypeConverters;
+
+namespace SubSonic.Configuration
+{
+    public class Configurator
+    {
+        private static volatile Configurator _instance;
+        private static readonly object SyncRoot = new object();
+        private IWindsorContainer _container;
+
+        private Configurator ()
+        {
+            InitializeContainer();
+        }
+
+        public static IWindsorContainer Container
+        {
+            get
+            {
+                if (_instance == null)
+                {
+                    lock (SyncRoot)
+                    {
+                        if (_instance == null)
+                        {
+                            _instance = new Configurator();
+                        }
+                    }
+                }
+                return _instance._container;
+            }
+        }
+
+        private void InitializeContainer()
+        {
+            var container = new WindsorContainer();
+            _container =
+                container.Register(
+                    Classes.FromThisAssembly()
+                           .BasedOn(typeof (IValueTypeConverter<,>))
+                           .WithService.DefaultInterfaces()
+                           .LifestyleTransient());
+        }
+    }
+}
---
+++ SubSonic.Core/TypeConverters/IValueTypeConverter.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using SubSonic.Configuration;
+
+namespace SubSonic.TypeConverters
+{
+    public interface IValueTypeConverter<TSourceType, TDestinationType>
+    {
+        TDestinationType Convert(TSourceType value);
+    }
+
+    public class StringToGuidValueTypeConverter : IValueTypeConverter<String, Guid>
+    {
+        public Guid Convert(string value)
+        {
+            return new Guid(value);
+        }
+    }
+
+    public class ValueTypeConverter
+    {
+        public static object ChangeType(Object value, Type conversionType)
+        {
+            try
+            {
+                var valueTypeConverterInterface = typeof(IValueTypeConverter<,>).MakeGenericType(value.GetType(), conversionType);
+                var valueTypeConverter = Configurator.Container.Resolve(valueTypeConverterInterface);
+                return valueTypeConverter.GetType().GetMethod("Convert").Invoke(valueTypeConverter, new[] {value});
+            }
+            catch (Castle.MicroKernel.ComponentNotFoundException)
+            {
+                return Convert.ChangeType(value, conversionType);   
+            }
+        }
+    }
+}
