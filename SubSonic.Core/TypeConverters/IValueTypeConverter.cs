b86c53d6597444f6368b95d87452caafe64c6c0f
Alonso Robles <alonso@alonsorobles.com>
2013-01-10 14:18:30 -0600

    Made PetaPoco aware of the ValueTypeConverterService and allows for last chance user defined data type handling. Fixes #13

--- SubSonic.Core/TypeConverters/IValueTypeConverter.cs
+++ SubSonic.Core/TypeConverters/IValueTypeConverter.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using SubSonic.Configuration;
+using SubSonic.Extensions;
 
 namespace SubSonic.TypeConverters
 {
@@ -16,19 +17,61 @@ namespace SubSonic.TypeConverters
         }
     }
 
+    public class DoubleToDecimalValueTypeConverter:IValueTypeConverter<Double, Decimal>
+    {
+        public decimal Convert(double value)
+        {
+            return new decimal(value);
+        }
+    }
+
     public class ValueTypeConverterService
     {
-        public static object ChangeType(Object value, Type conversionType)
+        public static object ChangeType(Object value, Type destinationType)
         {
             try
             {
-                var valueTypeConverterInterface = typeof(IValueTypeConverter<,>).MakeGenericType(value.GetType(), conversionType);
-                var valueTypeConverter = Configurator.Container.Resolve(valueTypeConverterInterface);
+                
+                var valueTypeConverter = LocateValueTypeConverter(value.GetType(), destinationType);
                 return valueTypeConverter.GetType().GetMethod("Convert").Invoke(valueTypeConverter, new[] {value});
             }
             catch (Castle.MicroKernel.ComponentNotFoundException)
             {
-                return Convert.ChangeType(value, conversionType);   
+                return Convert.ChangeType(value, destinationType);   
+            }
+        }
+
+        private static object LocateValueTypeConverter(Type sourceType, Type destinationType)
+        {
+            try
+            {
+                return TryLocateValueTypeConverter(sourceType, destinationType);
+                
+            }
+            catch (Castle.MicroKernel.ComponentNotFoundException)
+            {
+                return TryLocateValueTypeConverter(sourceType, destinationType.ToNotNullable());
+            }  
+        }
+
+        private static object TryLocateValueTypeConverter(Type sourceType, Type destinationType)
+        {
+            var valueTypeConverterInterface = typeof(IValueTypeConverter<,>).MakeGenericType(sourceType, destinationType);
+                return Configurator.Container.Resolve(valueTypeConverterInterface);
+        }
+
+        public static Func<object, object> ChangeTypeFunction(Type sourceType, Type destinationType, bool returnNullIfValueTypeConverterIsNotFound = false)
+        {
+            try
+            {
+                var valueTypeConverter = LocateValueTypeConverter(sourceType, destinationType);
+                return src => valueTypeConverter.GetType().GetMethod("Convert").Invoke(valueTypeConverter, new[] { src });
+            }
+            catch (Castle.MicroKernel.ComponentNotFoundException)
+            {
+                if (returnNullIfValueTypeConverterIsNotFound)
+                    return null;
+                return src => Convert.ChangeType(src, destinationType);
             }
         }
     }
